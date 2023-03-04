ffd91b6f48fdc0332051ea4aebec9fb1deec1e61
zoeyang2022 <zuyuyang@gmail.com>
2023-03-04 23:06:46 +1300

    delete duplicated files

--- Neisseria-genomes-used-for-graph/3ST_2K95.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-module load pggb/0.5.3
-
-input=$path
-
-
-
-pggb -i $input/3ST_NM_genomes.fa -s 2000 -p 95 -n 3 -t 16 -S -m -o 3ST_2K95_0.5.3
--- Neisseria-genomes-used-for-graph/get_distance_odgi_131NM0.4.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-module load odgi/0.7.3
-
-odgi paths -i ./131NM_10k95_0.4.gfa  -d > 131NM_10k95_0.4_distance 
-
-cut -f 1,2,6 131NM_10k95_0.4_distance >131NM_10k95_0.4_distance_cut
